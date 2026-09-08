e0b00369047e8ee41351d719da0b370d37120ec5
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-08 23:39:45 +0300

    Add TCP protocol compatibility header

---
+++ include/netinet/tcp.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef CHIMERA_NETINET_TCP_H
+#define CHIMERA_NETINET_TCP_H
+#include <stdint.h>
+#ifndef IPPROTO_TCP
+#define IPPROTO_TCP 6
+#endif
+#define TCP_NODELAY 1
+#define TCP_MAXSEG 2
+#define TCP_KEEPIDLE 4
+#define TCP_KEEPINTVL 5
+#define TCP_KEEPCNT 6
+#define TCP_SYNCNT 7
+#define TCP_LINGER2 8
+#define TCP_DEFER_ACCEPT 9
+#define TCP_WINDOW_CLAMP 10
+#define TCP_INFO 11
+#define TCP_QUICKACK 12
+#define TCP_CONGESTION 13
+#endif
