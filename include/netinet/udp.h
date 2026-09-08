2a278915f78f2585b79ee789b9d6b6fb3d81e40a
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-08 23:39:49 +0300

    Add UDP protocol compatibility header

---
+++ include/netinet/udp.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef CHIMERA_NETINET_UDP_H
+#define CHIMERA_NETINET_UDP_H
+#ifndef IPPROTO_UDP
+#define IPPROTO_UDP 17
+#endif
+#define UDP_CORK 1
+#define UDP_SEGMENT 103
+#define UDP_GRO 104
+#endif
