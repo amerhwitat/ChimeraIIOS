f0aac9ccaaee168ff648ea468c5abbb2537bfe37
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-10 11:05:36 +0300

    feat: expand filesystem matrix across Unix, legacy, network and foreign families

--- include/chimera/filesystem_matrix.hpp
+++ include/chimera/filesystem_matrix.hpp
@@ -19,7 +19,7 @@ struct FilesystemProfile {
     std::string_view family;
 };
 
-inline constexpr std::array<FilesystemProfile, 22> kFilesystemMatrix = {{
+inline constexpr std::array<FilesystemProfile, 48> kFilesystemMatrix = {{
     {"ext4", SupportMode::NativeReadWrite, "POSIX/VFS", "Linux"},
     {"xfs", SupportMode::NativeReadWrite, "POSIX/VFS", "Linux/SGI"},
     {"btrfs", SupportMode::NativeReadWrite, "POSIX/VFS", "Linux"},
@@ -41,7 +41,33 @@ inline constexpr std::array<FilesystemProfile, 22> kFilesystemMatrix = {{
     {"crossdos", SupportMode::UserspaceCompatibility, "AmigaDOS handler compatibility", "Amiga/DOS"},
     {"hp-ux-vxfs", SupportMode::UserspaceCompatibility, "HP-UX VxFS compatibility", "HP-UX"},
     {"openvms-ods2", SupportMode::UserspaceCompatibility, "RMS/Files-11 compatibility", "OpenVMS"},
-    {"openvms-ods5", SupportMode::UserspaceCompatibility, "RMS/Files-11 compatibility", "OpenVMS"}
+    {"openvms-ods5", SupportMode::UserspaceCompatibility, "RMS/Files-11 compatibility", "OpenVMS"},
+    {"hfs", SupportMode::NativeReadOnly, "POSIX/VFS", "Classic Mac"},
+    {"hfsplus", SupportMode::NativeReadWrite, "POSIX/VFS", "Apple"},
+    {"qnx6", SupportMode::UserspaceCompatibility, "QNX compatibility", "QNX"},
+    {"adfs", SupportMode::UserspaceCompatibility, "RISC OS compatibility", "Acorn/RISC OS"},
+    {"afs", SupportMode::UserspaceCompatibility, "AFS compatibility", "Unix/Network"},
+    {"9p", SupportMode::UserspaceCompatibility, "Plan 9 namespace compatibility", "Plan 9"},
+    {"nfs", SupportMode::NativeReadWrite, "Network VFS", "Unix"},
+    {"smb3", SupportMode::NativeReadWrite, "Network VFS", "Windows/Unix"},
+    {"gfs2", SupportMode::NativeReadWrite, "POSIX/VFS", "Linux cluster"},
+    {"ocfs2", SupportMode::NativeReadWrite, "POSIX/VFS", "Linux cluster"},
+    {"ceph", SupportMode::UserspaceCompatibility, "Network VFS", "Distributed"},
+    {"orangefs", SupportMode::UserspaceCompatibility, "Network VFS", "Distributed"},
+    {"overlayfs", SupportMode::NativeReadWrite, "Union VFS", "Linux"},
+    {"proc", SupportMode::NativeReadWrite, "Pseudo-filesystem", "Linux"},
+    {"sysfs", SupportMode::NativeReadWrite, "Pseudo-filesystem", "Linux"},
+    {"tmpfs", SupportMode::NativeReadWrite, "Memory VFS", "Unix/Linux"},
+    {"ramfs", SupportMode::NativeReadWrite, "Memory VFS", "Linux"},
+    {"ubifs", SupportMode::NativeReadWrite, "MTD/flash VFS", "Linux"},
+    {"jffs2", SupportMode::NativeReadWrite, "MTD/flash VFS", "Linux"},
+    {"bfs", SupportMode::NativeReadOnly, "POSIX/VFS", "Unix"},
+    {"minix", SupportMode::NativeReadWrite, "POSIX/VFS", "Unix/Linux"},
+    {"befs", SupportMode::NativeReadOnly, "POSIX/VFS", "BeOS"},
+    {"reiserfs", SupportMode::NativeReadOnly, "POSIX/VFS", "Linux"},
+    {"hamer2", SupportMode::UserspaceCompatibility, "BSD VFS compatibility", "DragonFly BSD"},
+    {"apfs", SupportMode::UserspaceCompatibility, "Apple filesystem compatibility", "Apple"},
+    {"qfs", SupportMode::UserspaceCompatibility, "Chimera compatibility placeholder", "Chimera"}
 }};
 
 class FilesystemMatrix {
