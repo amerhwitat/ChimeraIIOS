8d0df38ddb579fbf2a9607571faaa06ae7035edc
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-11 11:41:47 +0300

    Chimera II OS: add explicit MSVC Windows library linkage

---
+++ ISO-Tool/vcpp/windows_linkage.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+#include <shlobj.h>
+#include <commctrl.h>
+#include <shellapi.h>
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "advapi32.lib")
