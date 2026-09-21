06ae3cefa9adb580f5a4466cdca09c1113f8ca16
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-21 16:58:08 +0300

    sdk: add Chimera language SDK sources

---
+++ sdk/examples/java/HelloChimera.java
@@ -0,0 +1,2 @@
+import org.chimera.sdk.ChimeraRuntime;
+public class HelloChimera { public static void main(String[] a){System.out.println("Chimera SDK "+ChimeraRuntime.version()+" ("+ChimeraRuntime.targetTriple()+")");}}
